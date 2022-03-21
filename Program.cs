24f2c1bb56038fe86256a918efc4aa1fcf72cb39
rewyr <rewyr@LAPTOP-QUEA6UE5>
2022-03-21 08:57:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Method_Testing__2_
+{
+     class Program
+    {
+        static void Main
+            string s = "Brian";
+        int a = 20;
+         Console.WriteLine(   Compute (10, a +20)    ); //actual arguments  (paramters)
+            Console.WriteLine(compute (A*2, 10)); //actual arguments  (paramters)
+            Console.writeline(Compute (A*3,a00))  //actual arguments  (paramters)
+
+            int c = 50;
+        }
+
+
+    static internal Compute ( int a, int b)          // formal paramters
+    {
+        int result = a * 2 + b;
+        return result;
+    }
+
+    static internal Compute (int x, int y, int z)
+    {
+        return x + y + z;
+    }
+}
